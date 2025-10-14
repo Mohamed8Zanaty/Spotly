7e820ea9b37e691419ecda2f0c9c49d5c230abff
ibrahimragab2005 <ragabhima682@gmail.com>
2025-10-14 03:03:28 +0300

    Add Search screen

--- app/src/main/java/com/creator/spotly/MainActivity.kt
+++ app/src/main/java/com/creator/spotly/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.creator.spotly.ui.screens.SearchScreen
 import com.creator.spotly.ui.theme.SpotlyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,14 +23,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpotlyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
-            LoginScreen()
+
+            SearchScreen()
         }
     }
 }
