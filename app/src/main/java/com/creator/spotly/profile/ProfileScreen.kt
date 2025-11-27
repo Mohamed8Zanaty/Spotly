39afb91c1f1007f5a481be0437881ba23f0b109d
Ahmed Nagi <ahmednagymahdyfrg252@gmail.com>
2025-11-27 18:50:08 +0200

    searchFun

--- app/src/main/java/com/creator/spotly/profile/ProfileScreen.kt
+++ app/src/main/java/com/creator/spotly/profile/ProfileScreen.kt
@@ -1,4 +1,5 @@
 package com.creator.spotly
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -24,10 +25,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.creator.spotly.data.dto.UserProfileData
-import com.creator.spotly.ui.components.CustomIconButton
-import com.creator.spotly.ui.components.CustomTopbar
 import com.creator.spotly.profile.ProfileViewModel
 import com.creator.spotly.profile.components.OptionRow
+import com.creator.spotly.ui.components.CustomIconButton
+import com.creator.spotly.ui.components.CustomTopbar
 import com.creator.spotly.ui.theme.ButtonContainer
 import com.creator.spotly.ui.theme.OrangeRed
 
@@ -35,193 +36,156 @@ import com.creator.spotly.ui.theme.OrangeRed
 fun ProfileScreen(
     modifier: Modifier = Modifier,
     profileViewModel: ProfileViewModel = hiltViewModel(),
-    onBackClick: () -> Unit = {  },
-    onEditClick: () -> Unit = {  },
-    onLogoutClick: () -> Unit = {  },
-
-    ) {
-    val profile by profileViewModel.profile.collectAsState()
-    ProfileContent(
-        userProfile = profile,
-        onBackClick = onBackClick,
-        onEditClick = onEditClick,
-        onLogoutClick = onLogoutClick
-    )
+    onBackClick: () -> Unit = {},
+    onEditClick: () -> Unit = {},
+    onFavoriteClick: () -> Unit = {},
+    onPreviousTripsClick: () -> Unit = {},
+    onLogoutClick: () -> Unit = {},
+) {
+  val profile by profileViewModel.profile.collectAsState()
+  ProfileContent(
+      userProfile = profile,
+      onBackClick = onBackClick,
+      onEditClick = onEditClick,
+      onFavoriteClick = onFavoriteClick,
+      onPreviousTripsClick = onPreviousTripsClick,
+      onLogoutClick = onLogoutClick,
+  )
 }
 
 @Composable
 fun ProfileContent(
     userProfile: UserProfileData? = null,
-    onBackClick: () -> Unit = {  },
-    onEditClick: () -> Unit = {  },
-    onLogoutClick: () -> Unit = { }
+    onBackClick: () -> Unit = {},
+    onEditClick: () -> Unit = {},
+    onFavoriteClick: () -> Unit = {},
+    onPreviousTripsClick: () -> Unit = {},
+    onLogoutClick: () -> Unit = {},
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-    ) {
-        CustomTopbar(
-            title = "Profile",
-            backButtonHandler = onBackClick,
-            item = {
-                CustomIconButton(
-                    onClick = onEditClick,
-                    icon = Icons.Outlined.Edit,
-                    contentColor = OrangeRed,
+  Column(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+    CustomTopbar(
+        title = "Profile",
+        backButtonHandler = onBackClick,
+        item = {
+          CustomIconButton(
+              onClick = onEditClick,
+              icon = Icons.Outlined.Edit,
+              contentColor = OrangeRed,
+          )
+        },
+        backContainerColor = ButtonContainer,
+    )
 
-                )
-            },
-            backContainerColor = ButtonContainer
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.user),
-                contentDescription = "User Image",
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-            )
-        }
-        Spacer(modifier = Modifier.height(12.dp))
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(userProfile?.name ?: "Unknown", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            Text(userProfile?.email ?: "Unknown", color = Color.Gray, fontSize = 14.sp)
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
+    Spacer(modifier = Modifier.height(24.dp))
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+      Image(
+          painter = painterResource(id = R.drawable.user),
+          contentDescription = "User Image",
+          modifier = Modifier.size(100.dp).clip(CircleShape),
+      )
+    }
+    Spacer(modifier = Modifier.height(12.dp))
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+      Text(userProfile?.name ?: "Unknown", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+      Text(userProfile?.email ?: "Unknown", color = Color.Gray, fontSize = 14.sp)
+    }
+    Spacer(modifier = Modifier.height(24.dp))
+    Row(
+        modifier =
+            Modifier.fillMaxWidth()
                 .background(Color(0xFFF7F7F9), RoundedCornerShape(16.dp))
                 .padding(vertical = 16.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Column (
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ){
-                Text("Rewards Points", fontSize = 14.sp, color = Color.Black)
-                Text(userProfile?.rewardPoints.toString(), fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color(0xFFFF6421))
-            }
-            Divider(
-                color = Color(0xFFE0E0E0),
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(1.dp)
-            )
-            Column (
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text("Travel Trips", fontSize = 14.sp, color = Color.Black)
-                Text(userProfile?.travelTrips.toString(), fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color(0xFFFF6421))
-            }
-            Divider(
-                color = Color(0xFFE0E0E0),
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(1.dp)
-            )
-            Column (
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text("Bucket List", fontSize = 14.sp, color = Color.Black)
-                Text(userProfile?.bucketListItems.toString(), fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color(0xFFFF6421))
-            }
-        }
-        Spacer(modifier = Modifier.height(30.dp))
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+      Column(
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.spacedBy(10.dp),
+      ) {
+        Text("Rewards Points", fontSize = 14.sp, color = Color.Black)
+        Text(
+            userProfile?.rewardPoints.toString(),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFFF6421),
+        )
+      }
+      Divider(color = Color(0xFFE0E0E0), modifier = Modifier.height(50.dp).width(1.dp))
+      Column(
+          verticalArrangement = Arrangement.spacedBy(10.dp),
+          horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        Text("Travel Trips", fontSize = 14.sp, color = Color.Black)
+        Text(
+            userProfile?.travelTrips.toString(),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFFF6421),
+        )
+      }
+      Divider(color = Color(0xFFE0E0E0), modifier = Modifier.height(50.dp).width(1.dp))
+      Column(
+          verticalArrangement = Arrangement.spacedBy(10.dp),
+          horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        Text("Bucket List", fontSize = 14.sp, color = Color.Black)
+        Text(
+            userProfile?.bucketListItems.toString(),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFFF6421),
+        )
+      }
+    }
+    Spacer(modifier = Modifier.height(30.dp))
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
+    Column(
+        modifier =
+            Modifier.fillMaxWidth()
                 .padding(vertical = 16.dp)
                 .background(Color(0xFFF7F7F9), RoundedCornerShape(16.dp))
                 .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+      OptionRow(icon = Icons.Default.FavoriteBorder, title = "Favorite", onClick = onFavoriteClick)
+      Divider(
+          color = Color(0xFFE0E0E0),
+          thickness = 0.5.dp,
+          modifier = Modifier.padding(horizontal = 8.dp),
+      )
+      OptionRow(
+          icon = Icons.Default.Flight,
+          title = "Previous Trips",
+          onClick = onPreviousTripsClick,
+      )
+      Divider(
+          color = Color(0xFFE0E0E0),
+          thickness = 0.5.dp,
+          modifier = Modifier.padding(horizontal = 8.dp),
+      )
 
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-
-        ) {
-
-            OptionRow(
-                icon = Icons.Default.BookmarkBorder,
-                title = "Book Marks",
-                onClick = {  }
-            )
-            Divider(
-                color = Color(0xFFE0E0E0),
-                thickness = 0.5.dp,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            OptionRow(
-                icon = Icons.Default.Flight,
-                title = "Previous Trips",
-                onClick = {  }
-            )
-            Divider(
-                color = Color(0xFFE0E0E0),
-                thickness = 0.5.dp,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            OptionRow(
-                icon = Icons.Default.Settings,
-                title = "Settings",
-                onClick = {  }
-            )
-            Divider(
-                color = Color(0xFFE0E0E0),
-                thickness = 0.5.dp,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            OptionRow(
-                icon = Icons.Default.Info,
-                title = "Version Info",
-                onClick = {  }
-            )
-            Divider(
-                color = Color(0xFFE0E0E0),
-                thickness = 0.5.dp,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            OptionRow(
-                icon = Icons.Default.Logout,
-                title = "Logout",
-                onClick = { onLogoutClick() }
-            )
-            Divider(
-                color = Color(0xFFE0E0E0),
-                thickness = 0.5.dp,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
+      OptionRow(icon = Icons.Default.Logout, title = "Logout", onClick = { onLogoutClick() })
+      Divider(
+          color = Color(0xFFE0E0E0),
+          thickness = 0.5.dp,
+          modifier = Modifier.padding(horizontal = 8.dp),
+      )
     }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileScreenPreview() {
-    val fakeUser = UserProfileData(
-        uid = "123",
-        name = "Reyna Mohamed",
-        email = "Ellaithy@email.com",
-        avatar = null,
-        rewardPoints = 50,
-        travelTrips = 40,
-        bucketListItems = 200
-    )
+  val fakeUser =
+      UserProfileData(
+          uid = "123",
+          name = "Reyna Mohamed",
+          email = "Ellaithy@email.com",
+          avatar = null,
+          rewardPoints = 50,
+          travelTrips = 40,
+          bucketListItems = 200,
+      )
 
-    ProfileContent(
-        userProfile = fakeUser,
-        onBackClick = {},
-        onEditClick = {},
-        onLogoutClick = {}
-    )
+  ProfileContent(userProfile = fakeUser, onBackClick = {}, onEditClick = {}, onLogoutClick = {})
 }
